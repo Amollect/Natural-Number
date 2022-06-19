48c742f93d08a243dedbf6da5ab7e346b46feb4c
Amol Shinde <107493995+Amollect@users.noreply.github.com>
2022-06-19 12:16:15 +0530

    Add files via upload

---
+++ add_NaturalNum.py
@@ -0,0 +1,10 @@
+#Find addition of first ten natural numbers. That is, find addition of 
+#numbers from 1 to 10
+
+add=0
+count=1
+while count<=10:
+    add=add+count
+    count+=1
+print("***addition of numbers from 1 to 10 is****")
+print(add)
